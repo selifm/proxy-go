cb5ce8f75256ad645f4eeba543e5a09de12af0d5
selfim <455697968@qq.com>
2020-11-13 10:45:06 +0800

    模拟用户可以重连

--- server/user.go
+++ server/user.go
@@ -6,36 +6,38 @@ import (
 )
 
 func main() {
-
-	reconn := make(chan bool)
 	defer func() {
 		err := recover()
 		if err != nil {
 			fmt.Println(err)
 		}
 	}()
+	reconn := make(chan error)
 	for {
 		userClient, err := net.Dial("tcp", "127.0.0.1:5200")
 		if err != nil {
-			panic(err)
+			go func() {
+				reconn <- err
+			}()
 		}
 
 		var name string
 		for {
+			fmt.Println("请输入你要传输的数据:")
 			fmt.Scanln(&name)
 			if name != "" {
 				if _, err := userClient.Write([]byte(name)); err != nil {
 					go func() {
-						reconn <- true
+						reconn <- err
 					}()
-					panic(err)
+					break
 				}
 				data := make([]byte, 10240)
 				if n, err := userClient.Read(data); err != nil {
 					go func() {
-						reconn <- true
+						reconn <- err
 					}()
-					panic(err)
+					break
 				} else {
 					fmt.Println(string(data[:n]))
 				}
